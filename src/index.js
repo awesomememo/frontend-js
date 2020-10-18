9a9e1386ca3c0eccd3ce0bc26e47de76cce44ac6
Tiffany Gan <tiffany.gan2015@gmail.com>
2020-10-17 17:19:04 -0700

    implement user and login, closes #4 #5 (#11)
    * implement user and login, closes #4 #5
    
    * address comments

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import { LOCALSTORAGE_KEY } from './Constant';
+import IndexUI from '../src/ui/IndexUI';
+import UserService from './service/UserService';
+
+const userService = new UserService();
+const ui = new IndexUI(document);
+
+window.addEventListener('DOMContentLoaded', async () => {
+    const currUserId = localStorage.getItem(LOCALSTORAGE_KEY);
+    if (currUserId === null) {
+        return;
+    }
+    const currUser = await userService.getUserById(currUserId);
+    ui.logIn(currUser.username);
+});
+
+ui.logoutBtn.addEventListener('click', () => {
+    ui.logout();
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+});
---
+++ src/settings.js
@@ -0,0 +1,44 @@
+import { LOCALSTORAGE_KEY } from "./Constant";
+import SettingsUI from "../src/ui/SettingsUI";
+import UserService from "./service/UserService";
+
+const ui = new SettingsUI(document);
+const userService = new UserService();
+const currUserId = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+let currUser;
+
+let settingsChecked = null;
+
+window.addEventListener('DOMContentLoaded', async () => {
+  currUser = await userService.getUserById(currUserId);
+  ui.newUsername.value = currUser.username;
+});
+
+ui.saveChangesBtn.addEventListener("click", async (e) => {
+  if (settingsChecked) {
+    currUser.username = ui.newUsername.value;
+    currUser.password = ui.newPassword.value;
+    await userService.updateUser(currUserId, currUser);
+    settingsChecked = null;
+  } else if (settingsChecked === null) {
+    e.preventDefault();
+    if (ui.newUsername.value === "" || ui.newPassword.value === "") {
+      settingsChecked = false;
+      ui.saveChangesBtn.click();
+      return;
+    }
+
+    if (!UserService.validatePassword(ui.oldPassword.value, currUser.password)) {
+      ui.showPasswordAlert();
+      settingsChecked = false;
+      ui.saveChangesBtn.click();
+      return;
+    }
+
+    settingsChecked = true;
+    ui.saveChangesBtn.click();
+  } else {
+      e.preventDefault();
+      settingsChecked = null;
+  }
+});
