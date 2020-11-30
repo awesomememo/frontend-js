4fd8f7bc7da3ced8b6204b64e138d3e81aab01fd
Yu Gan <yugan2005@gmail.com>
2020-11-30 00:41:23 -0800

    update the endpoint

--- src/Constant.js
+++ src/Constant.js
@@ -1,5 +1,5 @@
 export const CURR_USER_KEY = "currUser";
 export const MEMORIZATION_DATES = [1, 2, 3, 5, 8, 13];
 export const SECONDS_IN_A_DAY = 1000 * 60 * 60 * 24;
-export const BASE_URL = "http://localhost:8080";
+export const BASE_URL = "http://smartrepetition.com:8080";
 export const MAX_SOUND_CHUNK_LENGTH = 100000
